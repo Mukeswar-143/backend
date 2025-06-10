498fb3cd83715bb073672dc084e16cd97316c0bb
Mukeswar <vasanamukeswar@gmail.com>
2025-06-10 16:23:12 -0700

    nothing

--- studentportal/src/main/java/net/student/studentportal/controller/StudentsController.java
+++ studentportal/src/main/java/net/student/studentportal/controller/StudentsController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/Students")
-@CrossOrigin(origins = "http://localhost:3000")  // React frontend origin
+//@CrossOrigin(origins = "http://localhost:3000")  // React frontend origin
 public class StudentsController {
 
     @Autowired
