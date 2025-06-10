a8004d96caae50308461258e4c15069f4579db05
Mukeswar <vasanamukeswar@gmail.com>
2025-06-10 11:13:49 -0700

    nothing

--- studentportal/src/main/java/net/student/studentportal/repository/StudentsRepository.java
+++ studentportal/src/main/java/net/student/studentportal/repository/StudentsRepository.java
@@ -1,4 +1,6 @@
 package net.student.studentportal.repository;
+import net.student.studentportal.entity.StudentsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentsRepository {
+public interface StudentsRepository extends JpaRepository<StudentsEntity, Long> {
 }
